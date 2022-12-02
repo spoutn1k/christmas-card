df119f2ee98b72b54ca2896dbb2150370fed9881
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2022-12-01 18:02:53 -0800

    Added card overlay

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-var stateData = {};
+var states = {};
 
 function fetchState(stateID, stateData) {
     let state = stateData.states[stateID];
@@ -22,10 +22,32 @@ function displayState(stateData) {
     });
 }
 
+function initStates() {
+    let initialState = fetchState(states.initial_state, states);
+    displayState(initialState);
+}
+
+function initCard() {
+    let cardHolder = document.querySelector("#cardholder");
+
+    let background = document.createElement('img');
+    background.src = `assets/${states.christmas_card.full}`;
+
+    cardHolder.appendChild(background);
+
+    for (dude in states.christmas_card.people) {
+        let mask = document.createElement('img');
+        mask.src = `assets/${states.christmas_card.people[dude].mask}`;
+
+        cardHolder.appendChild(mask);
+    }
+}
+
 fetch('./house.json')
     .then(response => response.json())
     .then(json => {
         states = json;
-        let initialState = fetchState(states.initial_state, states);
-        displayState(initialState);
+
+        initStates();
+        initCard();
     });
